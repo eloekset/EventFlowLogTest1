5451c7aaaf4830e71791c8beced048766e62c339
eloekset <eloekset@msn.com>
2018-02-02 15:23:46 +0100

    Add simplest possible config for ILogger input and StdOut output.

---
+++ EventFlowLogTest1/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Diagnostics.EventFlow;
+using Microsoft.Diagnostics.EventFlow.Inputs;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace EventFlowLogTest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var diagnosticPipeline = DiagnosticPipelineFactory.CreatePipeline("eventFlowConfig.json"))
+            {
+                //System.Diagnostics.Trace.TraceWarning("dsalkjfdlakdsølkj");
+                var loggerFactory = new LoggerFactory()
+                //.AddConsole(minLevel: Microsoft.Extensions.Logging.LogLevel.Trace)
+                //.AddDebug()
+                .AddEventFlow(diagnosticPipeline);
+
+                var logger = new Logger<Program>(loggerFactory);
+
+                using (logger.BeginScope("SomeScope"))
+                {
+                    logger.LogDebug("Log debug!");
+                    logger.LogInformation("Log information");
+                    logger.LogError("Log errrrrrr");
+                }
+            }
+        }
+    }
+}
